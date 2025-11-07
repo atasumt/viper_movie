12eda62ae600ce2f6ed23315fd1e749b2aea2336
atasumt <atasumt@gmail.com>
2025-11-07 22:12:40 +0300

    Initial Commit

---
+++ viper_movie/viper_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  viper_movieApp.swift
+//  viper_movie
+//
+//  Created by UMUT on 7.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct viper_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ viper_movieTests/viper_movieTests.swift
@@ -0,0 +1,17 @@
+//
+//  viper_movieTests.swift
+//  viper_movieTests
+//
+//  Created by UMUT on 7.11.2025.
+//
+
+import Testing
+@testable import viper_movie
+
+struct viper_movieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
